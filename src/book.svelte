9ba4cab0d53208654d2db981db4aab15c17dc60b
= <=>
2023-05-16 21:06:10 -0500

    Update

--- src/book.svelte
+++ src/book.svelte
@@ -8,11 +8,7 @@
   div{
       margin: 1rem;
       box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26)
-  }
-  h1{
-      font-size: 1.25rem;
-      margin: 0.25rem 0;
-  }
+  }  
   h2{
       font-size: 1rem;
       margin: 0.25rem 0;
